a76687a6c02a1f9a4105872f5649aa673a6753ed
Edmunds Folkmanis <folkmanis@gmail.com>
2022-10-18 11:10:23 +0300

    moved to lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './lib/material.module';
 
 
 @NgModule({
