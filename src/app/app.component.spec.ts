d441f2c0145093d06f53cb844618b9f4c3f6b267
Edmunds Folkmanis <folkmanis@gmail.com>
2022-10-18 10:19:11 +0300

    initial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'laiks'`, () => {
+  it(`should have as title 'Laiks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('laiks');
+    expect(app.title).toEqual('Laiks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('laiks app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Laiks app is running!');
   });
 });
