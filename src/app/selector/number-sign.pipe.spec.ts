cdb87f56e5b0b46b21fb4322bd1b9d731d77ad2a
Edmunds Folkmanis <folkmanis@gmail.com>
2022-10-19 12:34:06 +0300

    initial commit

---
+++ src/app/selector/number-sign.pipe.spec.ts
@@ -0,0 +1,39 @@
+import { NumberSignPipe } from './number-sign.pipe';
+
+describe('NumberSignPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NumberSignPipe();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('shold add "+" to positive number', () => {
+    const pipe = new NumberSignPipe();
+    const numb = 2;
+    expect(pipe.transform(numb)).toBe('+' + numb.toString());
+  });
+
+  it('should add "-" to negative number', () => {
+    const pipe = new NumberSignPipe();
+    const numb = -2;
+    expect(pipe.transform(numb)).toBe(numb.toString());
+  });
+
+  it('should output "0" on zero input', () => {
+    const pipe = new NumberSignPipe();
+    const numb = 0;
+    expect(pipe.transform(numb)).toBe('0');
+  });
+
+  it('should not modify string literals', () => {
+    const pipe = new NumberSignPipe();
+    const str = '2';
+    expect(pipe.transform(str)).toBe(str);
+  });
+
+  it('should not modify other objects', () => {
+    const pipe = new NumberSignPipe();
+    const str = { data: '2' };
+    expect(pipe.transform(str)).toEqual(str);
+  });
+
+});
---
+++ src/app/selector/number-sign.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'numberSign'
+})
+export class NumberSignPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    if (typeof value !== 'number') {
+      return value;
+    }
+    const str = value.toString();
+    return value > 0 ? '+' + str : str;
+  }
+
+}
