a1d9ecc3a8c6675e64d9f08c4fe3c9559a308cab
Edmunds Folkmanis <folkmanis@gmail.com>
2022-10-20 10:52:25 +0300

    strategy changed

--- src/app/selector/selector.component.spec.ts
+++ src/app/selector/selector.component.spec.ts
@@ -1,16 +1,38 @@
-import { ComponentFixture, TestBed, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 
 import { SelectorComponent } from './selector.component';
 
 import { SharedModule } from '../lib/shared.module';
+import { NumberSignPipe } from './number-sign.pipe';
+
+
+@Component({
+  template: `
+    <laiks-selector [value]="initialValue" (valueChanges)="onValueChanges($event)">
+    </laiks-selector>
+  `
+})
+class SelectorTestComponent {
+  initialValue: any = '2';
+  updatedValue: number | undefined;
+  onValueChanges(val: number) {
+    this.updatedValue = val;
+  }
+}
+
 
 describe('SelectorComponent', () => {
-  let component: SelectorComponent;
-  let fixture: ComponentFixture<SelectorComponent>;
+  let component: SelectorTestComponent;
+  let fixture: ComponentFixture<SelectorTestComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SelectorComponent],
+      declarations: [
+        SelectorComponent,
+        NumberSignPipe,
+        SelectorTestComponent,
+      ],
       imports: [SharedModule],
       providers: [
         { provide: ComponentFixtureAutoDetect, useValue: true },
@@ -18,7 +40,7 @@ describe('SelectorComponent', () => {
     })
       .compileComponents();
 
-    fixture = TestBed.createComponent(SelectorComponent);
+    fixture = TestBed.createComponent(SelectorTestComponent);
     component = fixture.componentInstance;
   });
 
@@ -41,50 +63,38 @@ describe('SelectorComponent', () => {
     expect(numberBlock).toBeTruthy();
   });
 
-  it('should initialy contain number in value', () => {
-    const value = component.value;
-    expect(typeof value).toBe('number');
-  });
-
-  it('should cast to number', () => {
-    const value = '-2';
-    expect(typeof component.value).toBe('number');
-  });
-
-  it('should display initial value', () => {
-    const numberBlock = fixture.nativeElement.querySelector('div.number') as HTMLDivElement;
-    expect(numberBlock.textContent).toContain(component.value);
+  it('should display positive values with plus sign', () => {
+    const { textContent } = fixture.nativeElement.querySelector('div.number') as HTMLDivElement;
+    expect(textContent).toContain('+2');
   });
 
   it('should add value on plus button press', () => {
     const button = fixture.nativeElement.querySelector('button#add') as HTMLButtonElement;
-    const oldValue = component.value;
     button.dispatchEvent(new Event('click'));
-    expect(component.value).toBe(oldValue + 1);
+    const { textContent } = fixture.nativeElement.querySelector('div.number') as HTMLDivElement;
+    expect(textContent).toContain('+3');
   });
 
   it('should subtract value on minus button press', () => {
     const button = fixture.nativeElement.querySelector('button#remove') as HTMLButtonElement;
-    const oldValue = component.value;
     button.dispatchEvent(new Event('click'));
-    expect(component.value).toBe(oldValue - 1);
+    button.dispatchEvent(new Event('click'));
+    button.dispatchEvent(new Event('click'));
+    const { textContent } = fixture.nativeElement.querySelector('div.number') as HTMLDivElement;
+    expect(textContent).toContain('-1');
+  });
+
+  it('should change value on input change', () => {
+    component.initialValue = -2;
+    fixture.detectChanges();
+    const { textContent } = fixture.nativeElement.querySelector('div.number') as HTMLDivElement;
+    expect(textContent).toContain('-2');
   });
 
   it('should emit value on button press', () => {
     const button = fixture.nativeElement.querySelector('button#add') as HTMLButtonElement;
-    component.valueChanges.subscribe(val => {
-      expect(val).toBe(component.value);
-    });
     button.dispatchEvent(new Event('click'));
-  });
-
-  it('should display updated value', () => {
-    const button = fixture.nativeElement.querySelector('button#add') as HTMLButtonElement;
-    const oldValue = component.value;
-    button.dispatchEvent(new Event('click'));
-    button.dispatchEvent(new Event('click'));
-    const { textContent } = fixture.nativeElement.querySelector('div.number') as HTMLDivElement;
-    expect(Number.parseFloat(textContent!)).toBe(oldValue + 2);
+    expect(component.updatedValue).toBe(3);
   });
 
 });
