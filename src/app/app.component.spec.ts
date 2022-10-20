a394850ac6cb6d9225b53aa49cde2a13dc26bb42
Edmunds Folkmanis <folkmanis@gmail.com>
2022-10-20 13:59:08 +0300

    initial working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { SelectorComponent } from './selector/selector.component';
 import { SharedModule } from './lib/shared.module';
 import { NumberSignPipe } from './selector/number-sign.pipe';
 import { ClockDisplayComponent } from './clock-display/clock-display.component';
 
+const TEST_TIME = new Date(2022, 10, 23, 21, 15, 0);
+const TEST_OFFSET = 3;
+const TEST_TIME_OFFSET = new Date(2022, 10, 24, 0, 15, 0);
+
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
       imports: [
         SharedModule,
@@ -18,20 +27,24 @@ describe('AppComponent', () => {
         ClockDisplayComponent,
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    component.currentTime = TEST_TIME;
+
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'Laiks'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Laiks');
+  it('should set time with offset', () => {
+    component.timeOffset = TEST_OFFSET;
+    fixture.detectChanges();
+    expect(component.timeWithOffset).toEqual(TEST_TIME_OFFSET);
   });
-
   /*   it('should render title', () => {
       const fixture = TestBed.createComponent(AppComponent);
       fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { OnInit, Component, ChangeDetectionStrategy } from '@angular/core';
+import { addHours } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,27 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'Laiks';
+export class AppComponent implements OnInit {
+
+  timeWithOffset!: Date;
+
+  currentTime: Date = new Date();
+
+  private _timeOffset = 0;
+  get timeOffset() {
+    return this._timeOffset;
+  }
+  set timeOffset(value: number) {
+    this._timeOffset = value;
+    this.updateTime();
+  }
+
+  ngOnInit(): void {
+    this.updateTime();
+  }
+
+  private updateTime() {
+    this.timeWithOffset = addHours(this.currentTime, this.timeOffset);
+  }
+
 }
