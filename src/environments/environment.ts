af8e4dab94d06c57430b159ed7218bed53d273a9
folkmanis <folkmanis@gmail.com>
2023-06-13 13:06:41 +0300

    moved to test firebase project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,13 @@
 
 export const environment = {
   firebase: {
-    projectId: 'laiks-e2d86',
-    appId: '1:548695761681:web:6e4dbffc785107976762b0',
-    storageBucket: 'laiks-e2d86.appspot.com',
-    locationId: 'europe-central2',
-    apiKey: 'AIzaSyBYkDsVPCgx-suAiDOG8mjtXYnJZLesJ40',
-    authDomain: 'laiks-e2d86.firebaseapp.com',
-    messagingSenderId: '548695761681',
-    measurementId: 'G-JKQS2F6Y1T',
+    apiKey: "AIzaSyBaO0VSrzz2z2VAxLhTn0bMI5dF8-OvYSM",
+    authDomain: "laiks-test.firebaseapp.com",
+    projectId: "laiks-test",
+    storageBucket: "laiks-test.appspot.com",
+    messagingSenderId: "892541170996",
+    appId: "1:892541170996:web:063f0ecee81d7ea277a161",
+    measurementId: "G-N433WCVQ32",
   },
   dbName: 'laiks',
   production: false
