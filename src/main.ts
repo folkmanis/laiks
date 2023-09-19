f98ba367253522f06bed3d5fed4d1000ee1f59ce
folkmanis <folkmanis@gmail.com>
2023-09-19 18:28:46 +0300

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, enableProdMode, importProvidersFrom } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeLv from '@angular/common/locales/lv';
-import { provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth } from '@angular/fire/auth';
-import { provideFirestore } from '@angular/fire/firestore';
+import {
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+  enableProdMode,
+  importProvidersFrom,
+} from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 import { APP_ROUTES } from './app/app-routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-
 
 if (environment.production) {
   enableProdMode();
@@ -29,16 +30,12 @@ bootstrapApplication(AppComponent, {
     { provide: LOCALE_ID, useValue: 'lv' },
     provideRouter(APP_ROUTES, withComponentInputBinding()),
     importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase))
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      BrowserAnimationsModule,
+      MatSnackBarModule,
+      MatDialogModule
     ),
-    importProvidersFrom(
-      provideAuth(() => getAuth())
-    ),
-    importProvidersFrom(
-      provideFirestore(() => getFirestore())
-    ),
-    importProvidersFrom(MatSnackBarModule),
-    importProvidersFrom(BrowserAnimationsModule),
-    importProvidersFrom(MatDialogModule),
   ],
 });
