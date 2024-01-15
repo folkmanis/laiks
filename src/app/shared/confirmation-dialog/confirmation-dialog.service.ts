e083e135825b8df7a011d241b1841f1a8afd1cad
folkmanis <folkmanis@gmail.com>
2024-01-15 19:54:03 +0200

    yes-no confirmation

--- src/app/shared/confirmation-dialog/confirmation-dialog.service.ts
+++ src/app/shared/confirmation-dialog/confirmation-dialog.service.ts
@@ -1,9 +1,10 @@
 import { ComponentType } from '@angular/cdk/portal';
 import { inject, Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { map, Observable } from 'rxjs';
 import { CancelEditConfirmationComponent } from './cancel-edit-confirmation/cancel-edit-confirmation.component';
 import { DeleteConfirmationComponent } from './delete-confirmation/delete-confirmation.component';
+import { YesNoConfirmationComponent } from './yes-no-confirmation/yes-no-confirmation.component';
 
 @Injectable({
   providedIn: 'root',
@@ -19,8 +20,17 @@ export class ConfirmationDialogService {
     return this.openComponent(CancelEditConfirmationComponent);
   }
 
-  private openComponent<T>(component: ComponentType<T>): Observable<boolean> {
-    const dialorgRef = this.dialog.open(component);
+  yesNoConfirmation(text: string, title: string) {
+    return this.openComponent(YesNoConfirmationComponent, {
+      data: { text, title },
+    });
+  }
+
+  private openComponent<T>(
+    component: ComponentType<T>,
+    config?: MatDialogConfig
+  ): Observable<boolean> {
+    const dialorgRef = this.dialog.open(component, config);
     return dialorgRef.afterClosed().pipe(map((resp) => !!resp));
   }
 }
