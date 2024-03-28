6ac8132decfda075c58cfe852e2a46ead6363fcf
folkmanis <folkmanis@gmail.com>
2024-03-28 13:45:32 +0200

    cleanup

--- src/app/shared/appliances/resolve-system-appliance.ts
+++ src/app/shared/appliances/resolve-system-appliance.ts
@@ -4,7 +4,6 @@ import { PowerAppliance } from '@shared/appliances';
 import { SystemAppliancesService } from './system-appliances.service';
 
 export const resolveSystemAppliance: ResolveFn<PowerAppliance> = (route) => {
-  return inject(SystemAppliancesService).getAppliance(
-    route.paramMap.get('id')!
-  );
+  return inject(SystemAppliancesService)
+    .getAppliance(route.paramMap.get('id')!);
 };
